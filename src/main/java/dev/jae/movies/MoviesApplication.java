c474448f39ac746e9c310fad3abd9f96dbb78fe7
JaeCreaConnects <jae.creaconnects@gmail.com>
2023-08-23 16:51:51 -0400

    cleanup

--- src/main/java/dev/jae/movies/MoviesApplication.java
+++ src/main/java/dev/jae/movies/MoviesApplication.java
@@ -1,21 +1,13 @@
 package dev.jae.movies;
 
-import com.mongodb.client.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
-		System.out.println("Hello, World!");
-	}
-	@Bean
-	public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-		return new MongoTemplate(mongoClient, "movie-api-db");
 	}
 
 }
