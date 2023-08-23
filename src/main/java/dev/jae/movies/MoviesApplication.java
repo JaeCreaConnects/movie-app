3e88c6afb21791cb45243d38fa4c78fe1cddb74c
JaeCreaConnects <jae.creaconnects@gmail.com>
2023-08-23 16:48:08 -0400

    attempt to fix test

--- src/main/java/dev/jae/movies/MoviesApplication.java
+++ src/main/java/dev/jae/movies/MoviesApplication.java
@@ -1,7 +1,10 @@
 package dev.jae.movies;
 
+import com.mongodb.client.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -9,6 +12,10 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 		System.out.println("Hello, World!");
+	}
+	@Bean
+	public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+		return new MongoTemplate(mongoClient, "movie-api-db");
 	}
 
 }
