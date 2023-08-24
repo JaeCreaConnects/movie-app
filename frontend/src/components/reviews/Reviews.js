14cff30806228c20d45df3b8c9aea0e68d4b9fe0
JaeCreaConnects <jae.creaconnects@gmail.com>
2023-08-23 20:29:41 -0400

    Add prettier to scripts and setup, run prettier

--- frontend/src/components/reviews/Reviews.js
+++ frontend/src/components/reviews/Reviews.js
@@ -1,96 +1,93 @@
-import React from 'react';
-import { useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
-import axios from "axios";
+import React, { useEffect, useRef } from 'react'
+import { useParams } from 'react-router-dom'
+import { Container, Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
-import ReviewForm from "./ReviewForm";
+import ReviewForm from './ReviewForm'
 
-
-const Reviews = ({getMovieData, movie, reviews, setReviews}) => {
-    const revText = useRef();
-    let params = useParams();
-    const movieId = params.movieId;
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+    const revText = useRef()
+    const params = useParams()
+    const movieId = params.movieId
 
     useEffect(() => {
-        getMovieData(movieId);
-    },[])
+        getMovieData(movieId)
+    }, [])
 
     const addReview = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
-        const rev = revText.current;
+        const rev = revText.current
 
-        try{
-            //TODO: pull this out to own api file
-            await axios.post("http://localhost:8080/api/v1/reviews", {reviewBody:rev.value, imdbId:movieId})
-            const updatedReviews = [...reviews, {body:rev.value}];
+        try {
+            // TODO: pull this out to own api file
+            await axios.post('http://localhost:8080/api/v1/reviews', {
+                reviewBody: rev.value,
+                imdbId: movieId,
+            })
+            const updatedReviews = [...reviews, { body: rev.value }]
 
-            rev.value = "";
+            rev.value = ''
 
-            setReviews(updatedReviews);
-
-        }
-        catch(err){
-            console.error(err);
+            setReviews(updatedReviews)
+        } catch (err) {
+            console.error(err)
         }
     }
 
-    return(
+    return (
         <Container>
             <Row>
                 <Col>
                     <h3>Reviews</h3>
                 </Col>
             </Row>
-            <Row className='mt-2'>
+            <Row className="mt-2">
                 <Col>
-                    <img src={movie?.poster} alt=''/>
+                    <img src={movie?.poster} alt="" />
                 </Col>
                 <Col>
                     {
                         <>
                             <Row>
                                 <Col>
-                                    <ReviewForm handleSubmit={addReview} revText={revText} labelText="Write a Review?"/>
+                                    <ReviewForm
+                                        handleSubmit={addReview}
+                                        revText={revText}
+                                        labelText="Write a Review?"
+                                    />
                                 </Col>
                             </Row>
 
                             <Row>
                                 <Col>
-                                    <hr/>
+                                    <hr />
                                 </Col>
                             </Row>
                         </>
                     }
-                    {
-                        reviews?.map((review) => {
-                            return(
-                                <>
-                                    <Row>
-                                        <Col>
-                                            {review.body}
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <hr/>
-                                        </Col>
-                                    </Row>
-
-                                </>
-                            )
-                        })
-                    }
+                    {reviews?.map((review) => {
+                        return (
+                            <>
+                                <Row>
+                                    <Col>{review.body}</Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <hr />
+                                    </Col>
+                                </Row>
+                            </>
+                        )
+                    })}
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <hr/>
+                    <hr />
                 </Col>
             </Row>
         </Container>
-
     )
 }
-export default Reviews;
+export default Reviews
