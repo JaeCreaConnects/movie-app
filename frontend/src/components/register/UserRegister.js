95f7735d3b56707672fc68f8d2e8b154cac28310
JaeCreaConnects <jae.creaconnects@gmail.com>
2023-08-29 13:06:19 -0400

    Add some css for fun

--- frontend/src/components/register/UserRegister.js
+++ frontend/src/components/register/UserRegister.js
@@ -1,6 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UserRegister = () => {
-    return <h1>Register User Page</h1>
+    const [firstName, setFirstName] = useState()
+    const [lastName, setLastName] = useState()
+    const [userName, setUserName] = useState()
+    const [password, setPassword] = useState()
+    const [email, setEmail] = useState()
+
+    return (
+        <div className="register-wrapper">
+            <h1>Please Log In</h1>
+            <form>
+                <label>
+                    <p>First Name</p>
+                    <input
+                        type="firstName"
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <p>Last Name</p>
+                    <input
+                        type="lastName"
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <p>Username</p>
+                    <input
+                        type="text"
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <label>
+                    <p>Email</p>
+                    <input
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 export default UserRegister
